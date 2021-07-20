ddc7d38743c7e968f05d92ef7c9c466e54065d9d
jc <SM@shed31>
2021-07-20 09:31:58 +0200

    first commit

---
+++ src/test/java/TourGuide/msgps/MsGpsApplicationTests.java
@@ -0,0 +1,13 @@
+package TourGuide.msgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
