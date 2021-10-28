9200339292d3822a0eafffc4b654a50627ca1804
Jean-César <jeancesar.bazin@gmail.com>
2021-10-28 14:04:27 +0200

    adding jacoco

--- src/test/java/dev/jcb/gps/MsGpsApplicationTests.java
+++ src/test/java/dev/jcb/gps/MsGpsApplicationTests.java
@@ -4,11 +4,13 @@ import dev.jcb.gps.consumer.RewardGateway;
 import dev.jcb.gps.service.GpsService;
 import gpsUtil.GpsUtil;
 import gpsUtil.location.VisitedLocation;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
+import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
@@ -18,17 +20,6 @@ class MsGpsApplicationTests {
 
     @Autowired
     GpsService gpsService;
-
-
-    @Test
-    public void getUserLocationTest() {
-        Locale.setDefault(new Locale("en", "US"));
-        GpsUtil gpsUtil = new GpsUtil();
-        RewardGateway rewardGateway = new RewardGateway();
-        GpsService gpsService = new GpsService(gpsUtil, rewardGateway);
-        VisitedLocation visitedLocation = gpsService.getUserLocation(UUID.fromString("2ac2923d-31a8-4ca3-a9d6-117bc50d86ed"));
-
-    }
 
 
 }
